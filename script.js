fceb80bbe5c4016e26c244b0ee38c4bde82eb21b
Justin Roy Tamaela <justin.roy.tamaela@mendix.com>
2021-07-09 11:22:49 +0200

    added functions to enter and render todos

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
-const tasks = ["first task", "second task", "third task"];
+// Selecting all html elements in variables
+const todoForm = document.querySelector(".todo-form");
+const todoInput = document.querySelector(".todo-input");
+const todoItemsList = document.querySelector(".todo-items");
+
+let todos = [];
+
+todoForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    addTodo(todoInput.value);
+});
+
+// add task to Array and clear input field
+function addTodo(item) {
+    if(item != '') {
+        const todo = {
+            id: Date.now(),
+            name: item,
+            completed: false,
+        };
+
+        todos.push(todo);
+        renderTodos(todos);
+        //console.log(todos);
+
+        todoInput.value = '';
+    }
+}
 
 
 //Print tasks on page
-const article = document.querySelector("article");
-let tasksList = document.createElement("ul");
+function renderTodos(todos) {
+    todoItemsList.innerHTML = '';
 
-for (let i = 0; i < tasks.length; i++) {
-    let listItem = document.createElement("li");
-    listItem.innerHTML = tasks[i];
-    tasksList.append(listItem);
-  }
-article.append(tasksList);
+    todos.forEach(function(item) {
+        const checked = item.completed ? 'checked' : null;
+
+        const li = document.createElement("li");
+        li.setAttribute("class", "item");
+        li.setAttribute("data-key", item.id);
+        if(item.completed == true) {
+            li.classList.add('checked');
+        }
+        li.innerHTML = `
+            <input type="checkbox" class="checkbox" ${checked}>
+            ${item.name}
+            <button class="delete-button">X</button>
+        `;
+        todoItemsList.append(li);
+    });
+};
