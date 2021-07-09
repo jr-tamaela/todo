54de29c06d47456c66b6e2fd9f362ff2beab6f55
Justin Roy Tamaela <justin.roy.tamaela@mendix.com>
2021-07-09 09:47:11 +0200

    added 'print task on page' function

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-console.log("hello world!");
-
-let mytodos = [];
-
-const todoItem = {
-    text: "",
-    checked: false,
-    id: Date.now(),
-};
+const tasks = ["first task", "second task", "third task"];
 
 
+//Print tasks on page
+const article = document.querySelector("article");
+let tasksList = document.createElement("ul");
+
+for (let i = 0; i < tasks.length; i++) {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = tasks[i];
+    tasksList.append(listItem);
+  }
+article.append(tasksList);
